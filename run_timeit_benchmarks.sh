ec8e831f2fc634891c1adcddb7db7835b4d72364
siddhesh <sid1793@gmail.com>
2026-01-21 23:50:07 -0800

    run each input 10 times

--- run_timeit_benchmarks.sh
+++ run_timeit_benchmarks.sh
@@ -1,5 +1,19 @@
-python python_timeit_benchmark.py --num_layers 12 --num_heads 12 --d_model 768 --d_ff 3072
-python python_timeit_benchmark.py --num_layers 24 --num_heads 16 --d_model 1024 --d_ff 4096
-python python_timeit_benchmark.py --num_layers 36 --num_heads 20 --d_model 1280 --d_ff 5120
-python python_timeit_benchmark.py --num_layers 48 --num_heads 25 --d_model 1600 --d_ff 6400
-python python_timeit_benchmark.py --num_layers 32 --num_heads 32 --d_model 2560 --d_ff 10240
+for i in {1..10}; do
+    python python_timeit_benchmark.py --num_layers 12 --num_heads 12 --d_model 768 --d_ff 3072
+done
+
+for i in {1..10}; do
+    python python_timeit_benchmark.py --num_layers 24 --num_heads 16 --d_model 1024 --d_ff 4096
+done
+
+for i in {1..10}; do
+    python python_timeit_benchmark.py --num_layers 36 --num_heads 20 --d_model 1280 --d_ff 5120
+done
+
+for i in {1..10}; do
+    python python_timeit_benchmark.py --num_layers 48 --num_heads 25 --d_model 1600 --d_ff 6400
+done
+
+for i in {1..10}; do
+    python python_timeit_benchmark.py --num_layers 32 --num_heads 32 --d_model 2560 --d_ff 10240
+done
