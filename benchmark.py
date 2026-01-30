2983a2298fa94125ad42f9f0dd571ab6c3a35877
siddhesh <sid1793@gmail.com>
2026-01-29 22:01:53 -0800

    wrap benchmarking script

--- benchmark.py
+++ benchmark.py
@@ -3,8 +3,39 @@ import argparse
 import torch
 import csv
 import os
+import math
+import torch.nn as nn
 from datetime import datetime
-from cs336_basics.transformers_arch import TransformerLM
+import torch.cuda.nvtx as nvtx
+from cs336_basics.transformers_training import AdamW
+from einops import einsum
+
+class AnnotatedScaledDotProductAttention(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.softmax = SoftMax()
+
+    def forward(self, Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor, mask: torch.Tensor = None) -> torch.Tensor:
+        with nvtx.range("scaled_dot_product_attention"):
+            d_k = Q.shape[-1]
+
+            with nvtx.range("attention_scores"):
+                attn_scores = einsum(Q, K, "... n d_k, ... m d_k -> ... n m")
+                attn_scores = attn_scores / math.sqrt(d_k)
+                if mask is not None:
+                    attn_scores.masked_fill_(~mask, -math.inf)
+            
+            with nvtx.range("softmax"):
+                attn_scores = self.softmax(attn_scores, dim=-1)
+
+            with nvtx.range("final_matmul"):
+                return einsum(attn_scores, V, "... n m, ... m d_v -> ... n d_v")
+
+import cs336_basics.transformers_arch
+
+cs336_basics.transformers_arch.ScaledDotProductAttention = AnnotatedScaledDotProductAttention
+
+from cs336_basics.transformers_arch import TransformerLM, SoftMax
 
 def main():
     parser = argparse.ArgumentParser()
@@ -26,32 +57,51 @@ def main():
       num_heads=args.num_heads,
       d_ff=args.d_ff,
       rope_theta=10_000).to('cuda') # Mocking rope_theta
+    
+    optim = AdamW(model.parameters())
 
     for i in range(args.warmup_steps):
-        input = torch.randint(0, args.vocab_size, (args.batch_size, args.context_length))
+        input = torch.randint(0, args.vocab_size, (args.batch_size, args.context_length)).to('cuda')
         _ = model(input)
 
-    input = torch.randint(0, args.vocab_size, (args.batch_size, args.context_length))
+    input = torch.randint(0, args.vocab_size, (args.batch_size, args.context_length)).to('cuda')
     start_time_forward = timeit.default_timer()
-    op = model(input)
-    torch.cuda.synchronize()
+    with nvtx.range("forward pass"):
+        op = model(input)
+        torch.cuda.synchronize()
     end_time_forward = timeit.default_timer()
 
     loss = op.mean()
     start_time_backward = timeit.default_timer()
-    loss.backward()
-    torch.cuda.synchronize()
+    with nvtx.range("backward pass"):
+        loss.backward()
+        torch.cuda.synchronize()
     end_time_backward = timeit.default_timer()
+
+    start_time_optim = timeit.default_timer()
+    with nvtx.range("optim step"):
+        optim.step()
+        torch.cuda.synchronize()
+    end_time_optim = timeit.default_timer()
+
+    model = model.eval()
+
+    with nvtx.range("forward no grad"):
+        with torch.no_grad():
+            op = model(input)
+        torch.cuda.synchronize()
 
     # Calculate elapsed times
     forward_time = end_time_forward - start_time_forward
     backward_time = end_time_backward - start_time_backward
+    optim_time = end_time_optim - start_time_optim
 
     # Build result dict
     result = vars(args).copy()
     result['timestamp'] = datetime.now().isoformat()
     result['forward_time'] = forward_time
     result['backward_time'] = backward_time
+    result['optim_time'] = optim_time
 
     # Print to stdout
     print(result)
